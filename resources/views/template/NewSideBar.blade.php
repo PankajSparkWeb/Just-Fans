9e7fe94f1943e509a89ee55492dabaf7fc8d397e
Mr-rakesh-rk <panku102001@gmail.com>
2024-01-04 16:49:59 +0530

    Update

---
+++ resources/views/template/NewSideBar.blade.php
@@ -0,0 +1,150 @@
+<div class="new-side-bar-container">
+    <div class="new-side-bar">
+        <div class="about-community-card">
+            <div class="about-comunity-text-area">
+                <div class="side-bar-heading">
+                    <h3>About Community</h3>
+                </div>
+                <div class="discription">
+                    <p>You learn something new every day; what did you learn today? Submit interesting and specific
+                        facts about something that you just found out here.</p>
+                    <p>Created Dec 28, 2008</p>
+                </div>
+            </div>
+            <div class="about-comunity-member-area">
+                <div class="mambers">
+                    <h4>1</h4>
+                    <p></p>
+                </div>
+                <div class="Online">
+                    <h4>2</h4>
+                    <p></p>
+                </div>
+                <div class="ranked-by-size">
+                    <h4>3</h4>
+                    <p></p>
+                </div>
+            </div>
+
+            <div class="about-comunity-createPost"><a href="">Create Post</a></div>
+            <div class="about-community-options">
+                <div class="accordion side-bar-accordian">
+                    <div class="accordion-item">
+                        <div class="accordion-header">Community Options</div>
+                        <div class="accordion-content">
+                            <li><a href="">Comunity Theame</a></li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="rules-card">
+            <div class="side-bar-heading">
+                <h3>Rules</h3>
+            </div>
+            <div class="accordion side-bar-accordian">
+                <div class="accordion-item">
+                    <div class="accordion-header">1. Inaccurate/unverifiable/not supported by source</div>
+                    <div class="accordion-content">
+                        <p>Please link directly to a reliable source that supports every claim in your post title.
+                            Images alone do not count as valid references. Videos are fine so long as they come from
+                            reputable sources (e.g. BBC, Discovery, etc).</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="accordion side-bar-accordian">
+                <div class="accordion-item">
+                    <div class="accordion-header">2. No personal opinions/anecdotes/subjective posts</div>
+                    <div class="accordion-content">
+                        <p>(e.g "TIL xyz is a great movie")</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="accordion side-bar-accordian">
+                <div class="accordion-item">
+                    <div class="accordion-header">3. No recent sources </div>
+                    <div class="accordion-content">
+                        <p>Any sources (blog, article, press release, video, etc.) more recent than two months are not
+                            allowed.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion side-bar-accordian">
+                <div class="accordion-item">
+                    <div class="accordion-header">4. No politics/agenda pushing</div>
+                    <div class="accordion-content">
+                        <p>No submissions regarding or related to the following: recent politics, politicians, police
+                            misconduct, race/religion/gender, environmental issues, social issues, etc. See wiki for
+                            more detailed explanation</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="accordion side-bar-accordian">
+                <div class="accordion-item">
+                    <div class="accordion-header">5. No misleading claims</div>
+                    <div class="accordion-content">
+                        <p>Posts that omit essential information, or present unrelated facts in a way that suggest a
+                            connection will be removed.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion side-bar-accordian">
+                <div class="accordion-item">
+                    <div class="accordion-header">6. Too general/can't stand on its own/how to</div>
+                    <div class="accordion-content">
+                        <p>a. Titles must begin with "TIL" or "Today I Learned" b. Make them descriptive, concise and
+                            specific (e.g. not "TIL something interesting about bacon"). c. Titles must be able to stand
+                            on their own without requiring readers to click on a link. d. "TIL about ..." and other
+                            broad posts don't belong on TIL. Try /r/Wikipedia, etc. instead, or be more specific (and
+                            avoid the word "about"). e. "TIL how to ..." posts belong on /r/HowTo.</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="accordion side-bar-accordian">
+                <div class="accordion-item">
+                    <div class="accordion-header">7. No submissions about software/websites</div>
+                    <div class="accordion-content">
+                        <p>No submissions related to the usage, existence or features of specific software/websites
+                            (e.g. "TIL you can click on widgets in WidgetMaker 1.22").</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <p>All NSFW links must be tagged.
+                </p>
+            </div>
+
+
+
+        </div>
+        <div class="wikipeadia-card">
+            <div class="side-bar-wiki-heading side-bar-heading">
+                <h3>Wiki</h3>
+                </div>
+            <div class="side-bar-wiki-discription">Please see the wiki for more detailed explanations of the rules as
+                well as additional rules that may not be listed here</div>
+        </div>
+
+
+        <div class="moderators-card">
+            <div class="side-bar-moderators-heading side-bar-heading">
+                <h3>Moderators</h3>
+                </div>
+            <div class="side-bar-discription"></div>
+        </div>
+    </div>
+</div>
